865a3ab240c9bf42a869eeb3273c7a1c73de74c1
Miha K <mihkopac@hotmail.com>
2024-07-01 20:15:35 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CharacterCountDirective } from './directives/character-count.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CharacterCountDirective
+    ],
+    imports: [
+        InputTextareaModule,
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/directives/character-count.directive.ts
@@ -0,0 +1,57 @@
+import { AfterViewInit, ChangeDetectorRef, Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { NgControl, NgModel } from '@angular/forms';
+import { InputTextarea } from 'primeng/inputtextarea';
+
+@Directive({
+  selector: '[appCharacterCount]'
+})
+export class CharacterCountDirective extends InputTextarea implements AfterViewInit {
+  maxLength: number;
+  showCounter = false;
+  counter: HTMLElement;
+  wrapper: HTMLElement;
+
+  constructor(el: ElementRef, ngModel: NgModel, control: NgControl, cd: ChangeDetectorRef, private renderer: Renderer2) {
+    super(el, ngModel, control, cd);
+    this.maxLength = el.nativeElement.maxLength;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.maxLength < 0) {
+      return;
+    }
+    this.counter = this.renderer.createElement('span');
+    this.renderer.addClass(this.counter, 'text-area-char-counter');
+
+    this.wrapper = this.renderer.createElement('div');
+    this.renderer.addClass(this.wrapper, 'text-area-counter-wrapper');
+
+    const parent = this.el.nativeElement.parentNode;
+    this.renderer.appendChild(this.wrapper, this.el.nativeElement);
+    this.renderer.appendChild(this.wrapper, this.counter);
+    this.renderer.appendChild(parent, this.wrapper);
+  }
+
+  onFocus(e: any): void {
+    super.onFocus(e)
+    this.showCounter = true;
+    this.setCounterContent();
+  }
+
+  onBlur(e: any): void {
+    super.onBlur(e);
+    this.showCounter = false;
+    this.setCounterContent();
+  }
+
+  updateState(): void {
+    super.updateState();
+    this.setCounterContent();
+  }
+
+  setCounterContent() {
+    if (this.maxLength > 0) {
+      this.counter.innerHTML = this.showCounter ? this.el.nativeElement.value.length + '/' + this.maxLength : '';
+    }
+  }
+}
